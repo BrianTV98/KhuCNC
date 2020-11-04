07d4039c44bf1adddc2a2edb57c1445b6c67c71a
Trần Văn Hiếu <49116308+tranhieudev@users.noreply.github.com>
2020-11-04 22:58:51 +0700

    Update RD_ViewModel.py

--- Source/web/web/model/RD_ViewModel.py
+++ Source/web/web/model/RD_ViewModel.py
@@ -1,7 +1,12 @@
+import  math
 class RD_ViewModel:
-    def __init__(self, TEN_DN, NAM, TY_LE_CHI_PHI_RD, TY_LE_DH_TREN_DH_THAM_GIA_RD, KINH_PHI):
+    def __init__(self, TEN_DN, NAM, TY_LE_CHI_PHI_RD, TY_LE_DH_TREN_DH_THAM_GIA_RD, KINH_PHI=0):
+        if math.isnan(float(TY_LE_CHI_PHI_RD)):
+            TY_LE_CHI_PHI_RD = 0
+        else:
+            self.TY_LE_CHI_PHI_RD = TY_LE_CHI_PHI_RD
         self.TEN_DN = TEN_DN
         self.NAM = NAM
-        self.TY_LE_CHI_PHI_RD = TY_LE_CHI_PHI_RD
+
         self.TY_LE_DH_TREN_DH_THAM_GIA_RD = TY_LE_DH_TREN_DH_THAM_GIA_RD
         self.KINHPHI = KINH_PHI
