e3b9a4984d3aadd06c3e0ccf940a7ce1c6cbb0fb
Phùng Chí Bảo <57556117+baophung445@users.noreply.github.com>
2020-11-05 10:34:10 +0700

    a
    a

--- Source/web/web/model/RD_ViewModel.py
+++ Source/web/web/model/RD_ViewModel.py
@@ -8,8 +8,8 @@ class RD_ViewModel:
             self.TY_LE_CHI_PHI_RD = 0
         else:
             self.TY_LE_CHI_PHI_RD = TY_LE_CHI_PHI_RD
+
         self.TEN_DN = TEN_DN
         self.NAM = NAM
-        self.TY_LE_CHI_PHI_RD = TY_LE_CHI_PHI_RD
         self.TY_LE_DH_TREN_DH_THAM_GIA_RD = TY_LE_DH_TREN_DH_THAM_GIA_RD
         self.KINHPHI = KINH_PHI
