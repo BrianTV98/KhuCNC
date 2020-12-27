7b3fd6279639eec410b71f566707fdc093f24189
baophung445 <baophung445@gmail.com>
2020-12-27 19:41:48 +0700

    a
    fix thống kê lĩnh vực CNC & SL

--- Source/web/web/contrains/base_url.py
+++ Source/web/web/contrains/base_url.py
@@ -1,14 +1,14 @@
 import pyodbc
 
-# base_url_model = "D:\\KhuCNC\\Source\\model"
-#
-# conn = pyodbc.connect('Driver={SQL Server};'  # connect with SQL server
-#                       'Server=DESKTOP-LAFLFDC;'
-#                       'Database=DUAN_KHUCNC;'
-#                       'Trusted_Connection=yes;')
+base_url_model = "D:\\DoAnTotNghiepHieu\\KhuCNC\\Source\\model"
 
-base_url_model = "E:\do_an_tot_nghiep\Source\model"
 conn = pyodbc.connect('Driver={SQL Server};'  # connect with SQL server
-                      'Server=DESKTOP-05ICLAS\SERVER2;'
+                      'Server=DESKTOP-LAFLFDC;'
                       'Database=DUAN_KHUCNC;'
                       'Trusted_Connection=yes;')
+
+# base_url_model = "E:\do_an_tot_nghiep\Source\model"
+# conn = pyodbc.connect('Driver={SQL Server};'  # connect with SQL server
+#                       'Server=DESKTOP-05ICLAS\SERVER2;'
+#                       'Database=DUAN_KHUCNC;'
+#                       'Trusted_Connection=yes;')
